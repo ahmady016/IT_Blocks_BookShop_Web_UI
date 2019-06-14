2c2a8566235252eba9cf5aea25d0da08e17e0f80
ahmady016 <ahmady09@gmail.com>
2019-06-14 15:04:26 +0200

    move AppComponent => root folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './root/app.component';
 
 @NgModule({
   declarations: [
