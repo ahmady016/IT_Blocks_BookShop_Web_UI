0598c03349e292ff05ad63f96b1f70ade074f953
ahmady016 <ahmady09@gmail.com>
2019-06-16 08:10:28 +0200

    finalize sign-up component, add date service

---
+++ src/app/services/date.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class DateService {
+  Days() {
+    let result: string[] = [];
+    for (let i = 1; i <= 31; i++)
+      result.push(i.toString());
+    return result;
+  }
+  Months() {
+    return [['1', 'Jan'], ['2', 'Feb'], ['3', 'Mar'], ['4', 'Apr'], ['5', 'May'], ['6', 'Jun'], ['7', 'Jul'], ['8', 'Aug'], ['9', 'Sep'], ['10', 'Oct'], ['11', 'Nov'], ['12', 'Dec']];
+  }
+  Years(backTo) {
+    let result: string[] = [];
+    const currYear = (new Date()).getFullYear(),
+      oldYear = currYear - backTo;
+    for (let i = currYear; i >= oldYear; i--)
+      result.push(i.toString());
+    return result;
+  }
+  public isValidDate(day: number, month: number, year: number): boolean {
+    const isLeapYear = (year) => ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+    if (day === 31 && (month === 2 || month === 4 || month === 6 || month === 9 || month === 11))
+      return false;
+    if (day === 30 && month === 2)
+      return false;
+    if (day === 29 && !isLeapYear(year))
+      return false;
+    return true;
+  }
+}
--- src/app/users/sign-up/sign.up.component.ts
+++ src/app/users/sign-up/sign.up.component.ts
@@ -1,11 +1,115 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { AuthService } from './../../services/auth.service';
+import { DateService } from './../../services/date.service';
+import { User } from '../../models/user.model';
 
 @Component({
-	selector:'sign-up',
-	templateUrl:'sign.up.component.html',
-	styleUrls:['sign.up.component.css']
+	selector: 'sign-up',
+	templateUrl: 'sign.up.component.html',
+	styleUrls: ['sign.up.component.css']
 })
-export class SignUpComponent{
-	constructor(){
+export class SignUpComponent implements OnInit {
+	// the form value
+	newUser: User;
+	// fill birth date options
+	days: string[];
+	months: Array<string>[];
+	years: string[];
+	// Angular Form and its Controls
+	signUpForm: FormGroup;
+	email: FormControl;
+	password: FormControl;
+	name: FormControl;
+	address: FormControl;
+	mobile: FormControl;
+	day: FormControl;
+	month: FormControl;
+	year: FormControl;
+	birthDate: FormControl; // hold the complete birthDate
+	gender: FormControl;
+
+	constructor(
+		private authSrv: AuthService,
+		private dateSrv: DateService,
+		private router: Router
+	) { }
+	private _initForm() {
+		// fill Date Options
+		this.days = this.dateSrv.Days();
+		this.months = this.dateSrv.Months();
+		this.years = this.dateSrv.Years(70);
+
+		// build Controls
+		this.email = new FormControl("", [Validators.required, Validators.email]);
+		this.password = new FormControl("", [Validators.required, Validators.minLength(6)]);
+		this.name = new FormControl("", Validators.required);
+		this.address = new FormControl("", Validators.required);
+		this.mobile = new FormControl("", Validators.required);
+		this.birthDate = new FormControl("", Validators.required);
+		this.gender = new FormControl("", Validators.required);
+		this.day = new FormControl("", Validators.required);
+		this.month = new FormControl("", Validators.required);
+		this.year = new FormControl("", Validators.required);
+
+		// build the form
+		this.signUpForm = new FormGroup({
+			email: this.email,
+			password: this.password,
+			name: this.name,
+			address: this.address,
+			mobile: this.mobile,
+			birthDate: this.birthDate,
+			gender: this.gender,
+			day: this.day,
+			month: this.month,
+			year: this.year
+		});
+	}
+	private _isValidDate() {
+		if (this.day.value && this.month.value && this.year.value)
+			return this.dateSrv.isValidDate(
+				+this.day.value,
+				+this.month.value,
+				+this.year.value
+			);
+		return false;
+	}
+
+	private _setBirthDate() {
+		// if the fullDate is valid
+		this._isValidDate()
+			? this.birthDate.setValue(`${this.day.value}/${this.month.value}/${this.year.value}`)
+			: this.birthDate.setValue('');
+		// mark the formControl as touched to enable error messages
+		this.birthDate.markAsTouched();
+	}
+	ngOnInit() {
+		// build the form and its controls
+		this._initForm();
+		// manually set form control value based on isValidDate checking method
+		this.day.valueChanges.subscribe(v => this._setBirthDate());
+		this.month.valueChanges.subscribe(v => this._setBirthDate());
+		this.year.valueChanges.subscribe(v => this._setBirthDate());
+	}
+	signUp() {
+		if (this.signUpForm.invalid)
+			return;
+		// delete the date parts field from the new customer [exist only for formControls]
+		delete this.signUpForm.value.day;
+		delete this.signUpForm.value.month;
+		delete this.signUpForm.value.year;
+		// add the new Customer
+		this.newUser = this.signUpForm.value;
+		this.authSrv.register(this.newUser)
+			.subscribe(
+				_ => void this.router.navigate(["/books"]),
+				console.log
+			);
+		// reset the form
+		this.signUpForm.reset();
+		this.birthDate.markAsUntouched();
 	}
 }
