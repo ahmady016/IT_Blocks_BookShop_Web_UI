9051733b0c1367ba3519b8a962c3f100a8d827e1
ahmady016 <ahmady09@gmail.com>
2019-06-21 03:17:05 +0200

    fix new book bug in book.form

--- src/app/books/book-form/book.form.component.ts
+++ src/app/books/book-form/book.form.component.ts
@@ -47,76 +47,71 @@ export class BookFormComponent implements OnInit {
 	) { }
 
 	ngOnInit(): void {
+		this._initForm();
 		this.route.params.subscribe(route => {
-			if (route.bookId) {
-				this.bookId = +route.bookId;
-				this.bookSrv.find(route.bookId)
-					.subscribe(
-						_ => {
-							// create the book Form with its values
-							this.createForm(this.bookSrv.currentBook);
-							// manually set publishedDate value on each date parts change
-							this.fields.day.valueChanges.subscribe(v => this._setPublishedDate());
-							this.fields.month.valueChanges.subscribe(v => this._setPublishedDate());
-							this.fields.year.valueChanges.subscribe(v => this._setPublishedDate());
-						},
-						console.log
-					);
-			}
 			if (route.type === 'view')
 				this.readonly = true;
 			else if (route.type === 'edit')
 				this.edit = true;
+
+			this.bookId = +route.bookId;
+			if (this.bookId) {
+				this.bookSrv.find(route.bookId)
+					.subscribe(
+						book => void this._setFormValues(book),
+						console.log
+					);
+			}
 		});
 	}
 
-	createForm(book: Book) {
+	private _createForm() {
 		// fill Date Options
 		this.days = this.dateSrv.Days();
 		this.months = this.dateSrv.Months();
 		this.years = this.dateSrv.Years(70);
 		// build form fields
 		this.fields.title = new FormControl(
-			{ value: (book) ? book.title : '', disabled: this.readonly },
+			{ value: '', disabled: this.readonly },
 			Validators.required
 		);
 		this.fields.subtitle = new FormControl(
-			{ value: (book) ? book.subtitle : '', disabled: this.readonly },
+			{ value: '', disabled: this.readonly },
 			Validators.required
 		);
 		this.fields.description = new FormControl(
-			{ value: (book) ? book.description : '', disabled: this.readonly })
+			{ value: '', disabled: this.readonly })
 
 		this.fields.thumbnailUrl = new FormControl(
-			{ value: (book) ? book.thumbnailUrl : '', disabled: this.readonly },
+			{ value: '', disabled: this.readonly },
 			Validators.required
 		);
 		this.fields.pageCount = new FormControl(
-			{ value: (book) ? book.pageCount : '', disabled: this.readonly },
+			{ value: '', disabled: this.readonly },
 			Validators.required
 		);
 		this.fields.inventoryCount = new FormControl(
-			{ value: (book) ? book.inventoryCount : '', disabled: this.readonly },
+			{ value: '', disabled: this.readonly },
 			Validators.required
 		);
 		this.fields.unitPrice = new FormControl(
-			{ value: (book) ? book.unitPrice : '', disabled: this.readonly },
+			{ value: '', disabled: this.readonly },
 			Validators.required
 		);
 		this.fields.day = new FormControl(
-			{ value: (book) ? new Date(book.publishedDate).getDate() : '', disabled: this.readonly },
+			{ value: '', disabled: this.readonly },
 			Validators.required
 		);
 		this.fields.month = new FormControl(
-			{ value: (book) ? new Date(book.publishedDate).getMonth() : '', disabled: this.readonly },
+			{ value: '', disabled: this.readonly },
 			Validators.required
 		);
 		this.fields.year = new FormControl(
-			{ value: (book) ? new Date(book.publishedDate).getFullYear() : '', disabled: this.readonly },
+			{ value: '', disabled: this.readonly },
 			Validators.required
 		);
 		this.fields.publishedDate = new FormControl(
-			{ value: (book) ? book.publishedDate : '', disabled: this.readonly },
+			{ value: '', disabled: this.readonly },
 			Validators.required
 		);
 		// build Angular Form
@@ -140,6 +135,29 @@ export class BookFormComponent implements OnInit {
 			: this.fields.publishedDate.setValue('');
 		// mark the formControl as touched to enable error messages
 		this.fields.publishedDate.markAsTouched();
+	}
+
+	private _initForm() {
+		// create the book Form with its values
+		this._createForm();
+		// manually set publishedDate value on each date parts change
+		this.fields.day.valueChanges.subscribe(v => this._setPublishedDate());
+		this.fields.month.valueChanges.subscribe(v => this._setPublishedDate());
+		this.fields.year.valueChanges.subscribe(v => this._setPublishedDate());
+	}
+
+	private _setFormValues(book: Book) {
+		this.fields.title.setValue(book.title)
+		this.fields.subtitle.setValue(book.subtitle)
+		this.fields.description.setValue(book.description)
+		this.fields.thumbnailUrl.setValue(book.thumbnailUrl)
+		this.fields.pageCount.setValue(book.pageCount)
+		this.fields.inventoryCount.setValue(book.inventoryCount)
+		this.fields.unitPrice.setValue(book.unitPrice)
+		this.fields.day.setValue(new Date(book.publishedDate).getDate() )
+		this.fields.month.setValue(new Date(book.publishedDate).getMonth() )
+		this.fields.year.setValue(new Date(book.publishedDate).getFullYear() )
+		this.fields.publishedDate.setValue(book.publishedDate)
 	}
 
 	save() {
