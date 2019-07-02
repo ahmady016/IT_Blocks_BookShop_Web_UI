7dba000a473aacff95844b0c471763c718870c85
ahmady016 <ahmady09@gmail.com>
2019-07-02 22:15:09 +0200

    show/hide navItems in small size screens

--- src/app/root/header/header.component.ts
+++ src/app/root/header/header.component.ts
@@ -9,13 +9,23 @@ import { AuthService } from 'src/app/_services';
 	styleUrls: ['header.component.css']
 })
 export class HeaderComponent {
+
 	title: string = 'IT Blocks BookShop'
+	showNavItems: boolean = true
+
 	constructor(
 		private authSrv: AuthService,
 		private router: Router
 	) { }
+
 	signOut() {
 		this.authSrv.logout();
 		this.router.navigate(["/sign-in"]);
 	}
+
+	navItemsToggle() {
+		this.showNavItems = !this.showNavItems;
+		console.log("TCL: HeaderComponent -> navItemsToggle -> showNavItems", this.showNavItems)
+	}
+
 }
